709cc477f44f77e3837c4995955262da5b08aa14
bkk <bkarsten@unitybox.de>
2025-04-05 13:35:33 +0200

    feat: generalise flashcard metadata

--- src/Question.cpp
+++ src/Question.cpp
@@ -10,20 +10,18 @@ void Question::setContentTextForm(const QString &textForm) {
 
 boost::json::object Question::questionToJson() {
     boost::json::object json;
-    json["proficiency"] = proficiency;
     json["customWeight"] = customWeight;
+    json["flashcardMetadata"] = fcMetadata;
     json["lastAnswered"] = lastAnswered.toString().toStdString();
-    json["lastCorrectlyAnswered"] = lastCorrectlyAnswered.toString().toStdString();
     json["lastChanged"] = lastChanged.toString().toStdString();
     json["content"] = getContentTextForm().toStdString();
     return json;
 }
 
 void Question::questionLoadJson(boost::json::object json) {
-    proficiency = json["proficiency"].as_double();
     customWeight = json["customWeight"].as_double();
+    fcMetadata = json["flashcardMetadata"].as_object();
     lastAnswered = QDateTime::fromString(QString::fromStdString(std::string(json["lastAnswered"].as_string())), Qt::ISODate);
-    lastCorrectlyAnswered = QDateTime::fromString(QString::fromStdString(std::string(json["lastCorrectlyAnswered"].as_string())), Qt::ISODate);
     lastChanged = QDateTime::fromString(QString::fromStdString(std::string(json["lastChanged"].as_string())), Qt::ISODate);
     setContentTextForm(QString::fromStdString(std::string(json["content"].as_string())));
 }
