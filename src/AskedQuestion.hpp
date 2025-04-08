0cf0bbde5fc5b5560f524a8971253692c585bfe7
bkk <118755879+bkkarsten@users.noreply.github.com>
2025-04-08 15:56:12 +0200

    feat: add asked question struct

---
+++ src/AskedQuestion.hpp
@@ -0,0 +1,20 @@
+#ifndef ASKEDQUESTION_H
+#define ASKEDQUESTION_H
+
+#include "Question.hpp"
+
+enum Direction {
+    FORWARD,
+    BACKWARD
+};
+
+/**
+ * @brief Includes a question and the direction in which it is asked
+ */
+struct AskedQuestion {
+    Question* question;
+    Direction direction;
+};
+
+
+#endif
