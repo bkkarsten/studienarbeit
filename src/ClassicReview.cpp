2277c6fbeaf2903ffc7b9fcfe4c2c2d3fd1bf007
bkk <118755879+bkkarsten@users.noreply.github.com>
2025-04-08 13:41:58 +0200

    feat: add classic review model

---
+++ src/ClassicReview.cpp
@@ -0,0 +1,35 @@
+#include "ClassicReview.hpp"
+
+#include <cstdlib>
+
+void ClassicReview::initialise(const KnowledgeGraph& graph) {
+    openQuestions = graph.allQuestions().values();
+    openQuestions.removeIf([&](Question* q) {
+        return !fcAlgo.due(q);
+    });
+}
+
+Question* ClassicReview::nextQuestion() {
+    if(openQuestions.isEmpty()) {
+        if(againQuestions.isEmpty()) {
+            return nullptr;
+        }
+        openQuestions = againQuestions;
+        againQuestions.clear();
+    }
+    int randomIndex = rand() % openQuestions.size();
+    Question* next = openQuestions.takeAt(randomIndex);
+    currentQuestion = next;
+    return next;
+}
+
+void ClassicReview::answerQuestion(unsigned int quality) {
+    if(currentQuestion == nullptr) {
+        return;
+    }
+    bool again = fcAlgo.answered(currentQuestion, quality);
+    if(again) {
+        againQuestions.append(currentQuestion);
+    }
+    currentQuestion = nullptr;
+}
---
+++ src/ClassicReview.hpp
@@ -0,0 +1,30 @@
+#ifndef CLASSICREVIEW_H
+#define CLASSICREVIEW_H
+
+#include <QList>
+
+#include "ReviewModel.hpp"
+
+/**
+ * @brief the classic review model, where all due questions are asked in random order.
+ */
+class ClassicReview : public ReviewModel {
+
+private:
+
+    Question* currentQuestion;
+    QList<Question*> openQuestions;
+    QList<Question*> againQuestions;
+
+public:
+
+    ClassicReview(const FlashcardAlgorithm& algorithm) : ReviewModel(algorithm) {}
+
+    void initialise(const KnowledgeGraph&) override;
+    Question* nextQuestion() override;
+    void answerQuestion(unsigned int) override;
+
+};
+
+
+#endif
